d8331ca9d214b35a5c2f70cb35a18862d838e8f7
Mehedi Hasan <44167334+ProgrammerHasan@users.noreply.github.com>
2025-04-14 21:10:31 +0600

    :sparkles: 🚀

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -6,7 +6,7 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b20434","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b20434","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"./app/main.dart.js"}]};
 
 
 _flutter.loader.load({
